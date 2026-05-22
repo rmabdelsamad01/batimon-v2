ff83407453b0a3c1d0eb785a549f8bd3de68fa9f
rmabdelsamad01 <rmabdelsamad01@gmail.com>
2026-05-22 14:49:39 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ let sbUser = null;
 let sbProfile = null;
 
 const PROJECT_META = {
-  'shift-tower':     {name:'Shift Tower',     active:true,  members:['raed','anas','nabil']},
-  'tanger-med':      {name:'Tanger MED',      active:false, members:['raed']},
+  'shift-tower':     {name:'Shift Tower',     active:true,  members:['raed']},
+  'tanger-med':      {name:'Tanger MED',      active:false, members:['nabil']},
   'riad-el-andalous':{name:'Riad El Andalous', active:false, members:['anas']},
   'anp':             {name:'ANP',             active:false, members:['nabil']},
-  'taghazout':       {name:'Taghazout',       active:false, members:['raed','anas']},
-  'casaone':         {name:'Casaone',         active:false, members:['nabil']},
+  'taghazout':       {name:'Taghazout',       active:false, members:['anas']},
+  'casaone':         {name:'Casaone',         active:false, members:['raed']},
 };
