b87e1db53a14f6f145b54111f8e176fd1c0f5bf2
rmabdelsamad01 <rmabdelsamad01@gmail.com>
2026-06-07 13:11:17 +0100

    Remove ANP and Tanger MED from PROJECT_META

--- config.js
+++ config.js
@@ -7,9 +7,7 @@ let sbProfile = null;
 
 const PROJECT_META = {
   'shift-tower':     {name:'Shift Tower',     active:true,  members:['raed']},
-  'tanger-med':      {name:'Tanger MED',      active:false, members:['nabil']},
   'riad-el-andalous':{name:'Riad El Andalous', active:false, members:['anas']},
-  'anp':             {name:'ANP',             active:false, members:['nabil']},
   'taghazout':       {name:'Taghazout',       active:false, members:['anas']},
   'casaone':         {name:'Casaone',         active:false, members:['raed']},
 };
